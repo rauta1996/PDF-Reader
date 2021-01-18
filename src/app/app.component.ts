1f364a6601645e7fbeecd8b4cdf183ad86d45266
Rajesh Rauta <rautarajesh19@gmail.com>
2021-01-19 00:55:46 +0530

    firsrt commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+// app.component.ts
+import { Component, ViewChild } from '@angular/core';
+import { PdfViewerComponent } from 'ng2-pdf-viewer';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'PDF-Reader';
+  title = 'angular-pdf-viewer-app';
+  pdfSrc="";
+
+  @ViewChild(PdfViewerComponent) 
+  renderText = true;
+  originalSize = false;
+
+  showAll = true;
+  autoresize = false;
+  showBorders = true;
+  renderTextModes = [0, 1, 2];
+  renderTextMode = 1;
+  rotation = 0;
+  zoom = 1;
+  zoomScale = 'page-width';
+  zoomScales = ['page-width', 'page-fit'];
+  pdfQuery = '';
+  totalPages: number[]=[];
+
+  zoomIn() {
+    this.zoom += 0.05;
+  }
+
+  zoomOut() {
+    if (this.zoom > 0.05)
+      this.zoom -= 0.05;
+  }
+
+  rotateDoc() {
+    this.rotation += 90;
+  }
+
+
+  // Event handler when new PDF file is selected
+  onFileSelected() {
+    const $pdf: any = document.querySelector('#file');
+
+    if (typeof FileReader !== 'undefined') {
+      const reader = new FileReader();
+
+      reader.onload = (e: any) => {
+        this.pdfSrc = e.target.result;
+      };
+
+      reader.readAsArrayBuffer($pdf.files[0]);
+    }
+  }
+
+  callBackFn(event: any) {
+    console.log('callBackFn', event);
+    // Setting total number of pages
+    this.totalPages = event._pdfInfo.numPages
+  }
+  pageRendered(event: any) {
+    console.log('pageRendered', event);
+  }
+  textLayerRendered(event: any) {
+    console.log('textLayerRendered', event);
+  }
+  onError(event: any) {
+    console.error('onError', event);
+  }
+  onProgress(event: any) {
+    console.log('onProgress', event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
+// app.module.ts
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { FormsModule } from '@angular/forms'
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    PdfViewerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
